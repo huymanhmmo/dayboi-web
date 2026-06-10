e3a13cf62302881d2dedf47f7d369bf4ea520101
huymanhmmo <huymanh.mmo@gmail.com>
2026-06-10 22:52:47 +0700

    Initial commit for dayboi-web with redesigned heroes and removed badges

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://dayboi.vip',
+  redirects: {
+    '/dia-diem/ha-noi': '/hoc-boi-ha-noi',
+    '/dia-diem/tphcm': '/hoc-boi-tphcm',
+  },
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      filter: (page) => !page.includes('/blog/the/') && !page.includes('/blog/danh-muc/'),
+    }),
+  ],
+  build: {
+    assets: '_assets',
+  },
+  image: {
+    domains: [],
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+    },
+  },
+});
